8dccae3f2ebf9788c0ea4448fa7b9ff532e33759
Nithin <nithin.linkin@gmail.com>
2019-02-11 23:50:57 +0530

    [theme] Add the gemmafy macro

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 // import Hello from './hello.green'
 // import Hello from './hello.white'
 import Hello from './hello'
+import gemmafy from './gemmafy.macro'
 
 class App extends Component {
     render() {
+        console.log(gemmafy("this is a pup load of mistakes"))
         return (
             <div className="App">
                 <Hello />
